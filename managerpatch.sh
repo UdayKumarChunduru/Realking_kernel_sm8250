f4f1b03b60e16371f728582de85391469e798a40
Uday Kumar Chunduru <40311746+UdayKumarChunduru@users.noreply.github.com>
2025-10-09 02:45:57 +0530

    Update managerpatch.sh

--- managerpatch.sh
+++ managerpatch.sh
@@ -1,4 +1,4 @@
 rm -rf KernelSU-Next
 echo "🔧 Setting up KernelSU-Next"
-curl -LSs "https://raw.githubusercontent.com/UdayKumarChunduru/KernelSU-Next/refs/heads/next-susfs-155-157-kpm/kernel/setup.sh" | bash -s next-susfs-155-157-kpm
+curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/susfs/kernel/setup.sh" | bash -s susfs
 echo "✅ KernelSU-Next setup completed"
